ecf54bc49ba816ed43698898ac3db369caadbbf3
asmirnov <daggett206@gmail.com>
2023-06-09 16:43:43 +0400

    just the case on which you can see the issue with async operations inside handler

--- src/track.ts
+++ src/track.ts
@@ -2,15 +2,24 @@ import { MCEvent } from '@managed-components/types'
 import { DemaPayload } from './types'
 
 export const track = async (event: MCEvent, payload: DemaPayload) => {
+  console.log('Run track', event, payload)
+
   const url = new URL('https://tracker.dema.ai/track.js')
 
   for (const [key, value] of Object.entries(payload)) {
     url.searchParams.set(key, value)
   }
 
-  return fetch(url.toString(), {
-    headers: {
-      'User-Agent': event.client.userAgent,
-    },
-  })
+  // console.log('Track: ' + url.toString())
+
+  // let's use API that works 100% just for testing
+  console.log('Track:', 'jsonplaceholder')
+
+  // return fetch(url.toString(), {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(response => {
+      console.log('--> Here is the response we wont see', response)
+      return response
+    })
 }
